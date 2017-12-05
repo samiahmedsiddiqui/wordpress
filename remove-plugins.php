01d755a466c718772ff0b82ee91eb5630af766f9
samiahmedsiddiqui <sami.siddiqui@yasglobal.com>
2017-12-05 11:36:54 +0500

    Added Search Example and fixed indent issues

--- remove-plugins.php
+++ remove-plugins.php
@@ -2,19 +2,19 @@
 
 /**
  * Place this under mu-plugins or create a new file under mu-plugins and remove the unnessary plugins on the specific pages/templates
- * 
+ *
  * In this example, i have only removed the si-contact-form from the Front Page. This can be remove from multiple pages/templates or multiple Plugins can be removed/disabled
  *
  * All depends on the requirement
  */
-function wp_remove_plugins($plugins){
+function yasglobal_remove_plugins( $plugins ) {
   if ( isset( $_SERVER['REQUEST_URI'] ) ) :
-    if( $remove_plugins_url === '/' ) {
-      $key = array_search( 'si-contact-form/si-contact-form.php' , $plugins ); // It searches specific plugin whether it's active or not. 
+    if ( $remove_plugins_url === '/' ) {
+      $key = array_search( 'si-contact-form/si-contact-form.php' , $plugins ); // It searches specific plugin whether it's active or not.
 	    if ( false !== $key ) { // If the above condition returns true so, it removes/unset the plugin or disable the plugin
 		    unset( $plugins[$key] );
 			}
-    }  
+    }
   endif;
 }
-add_filter( 'option_active_plugins', 'wp_remove_plugins' );
+add_filter( 'option_active_plugins', 'yasglobal_remove_plugins' );
---
+++ search-template.php
@@ -0,0 +1,13 @@
+<?php 
+
+/**
+ * Change the default template to the custom template for Search.
+ */
+function yasglobal_search_template( $template ) {
+	global $wp_query;
+	if ( ! $wp_query->is_search )
+		return $template;
+
+	return dirname( __FILE__ ) . '/search-page.php';
+}
+add_filter( 'template_include', 'yasglobal_search_template' );
